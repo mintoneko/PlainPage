abc108686abb79df9cc4bf00c84eb0d5d1620dcd
taojiacheng <swkzymlyy@gmail.com>
2024-12-20 16:15:00 +0800

    feat: first push

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://churchTao.github.io",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "min-light",
+        dark: "github-dark",
+      },
+      wrap: true,
+    },
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+// https://www.fastemote.com/pure-awesome-power 颜文字 (. ❛ ᴗ ❛.)
+export const SITE_TITLE = "PlainPage";
+export const SITE_DESCRIPTION = "Welcome (. ❛ ᴗ ❛.)";
+export const COPYRIGHT = "© 2024 PlainPage All Rights Reserved";
+// export const ICP_NUMBER = "";
