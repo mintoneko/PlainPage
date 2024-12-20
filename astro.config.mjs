cf010e40b6ad17b991ae7975e1acc6a59ba7b59f
taojiacheng <swkzymlyy@gmail.com>
2024-12-20 16:26:12 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://churchTao.github.io",
+  // 部署到 github pages 时，需要设置 base, 自己使用时，可以酌情设置
+  base: "/PlainPage",
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
