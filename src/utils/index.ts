eb6abf9e24614c56cd749e11850d7bf52403203e
taojiacheng <swkzymlyy@gmail.com>
2024-12-26 18:01:13 +0800

    fix: 空格和/

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -27,11 +27,24 @@ export const pathEqual = (a: string, b: string) => {
   return trimSlash(a) === trimSlash(b);
 };
 
+// 添加标签编码和解码函数
+export function encodeTag(tag: string) {
+  return tag
+    .replace(/\//g, "%2F") // 确保 / 被正确编码
+    .replace(/\s+/g, "-"); // 空格转换为 -
+}
+
+export function decodeTag(encodedTag: string) {
+  return decodeURIComponent(encodedTag);
+}
+
 // 平铺标签并去重
 export const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
   const tags: string[] = posts
     .filter(post => !post.data.hide)
-    .flatMap(post => post.data.tags);
+    .flatMap(post => post.data.tags)
+    // 进行url编码
+    .map(tag => encodeTag(tag));
   return [...new Set(tags)];
 };
 
@@ -39,5 +52,7 @@ export const getPostsByTag = (
   posts: CollectionEntry<"blog">[],
   tag: string
 ) => {
-  return posts.filter(post => !post.data.hide && post.data.tags.includes(tag));
+  return posts.filter(
+    post => !post.data.hide && post.data.tags.includes(decodeTag(tag))
+  );
 };
