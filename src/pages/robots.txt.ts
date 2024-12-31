5b508b1bd0e96466dad789e877038ec547daeff1
taojiacheng <swkzymlyy@gmail.com>
2024-12-31 17:02:04 +0800

    fix: SEO

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from "astro";
 
-const getRobotsTxt = (sitemapURL: URL) => `
-User-agent: *
+const getRobotsTxt = (sitemapURL: URL) => `User-agent: *
 Allow: /
+
 Sitemap: ${sitemapURL.href}
 `;
 
